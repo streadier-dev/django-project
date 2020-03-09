a865b8b7548620cc23b88c12c380f60288a1ea6b
gabriel <gabriel.faure@readinessit.com>
2020-03-09 16:16:04 -0300

    split number list and return like a json

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -19,4 +19,5 @@ from django.urls import path
 
 urlpatterns = [
     path('hello-world/', views.hello_world),
+    path('hi/', views.hi),
 ]
--- platzigram/views.py
+++ platzigram/views.py
@@ -1,7 +1,17 @@
-from django.http import HttpResponse
-
+from django.http import HttpResponse, JsonResponse
+import json
 from datetime import datetime
 
 def hello_world(request):
-    now = datetime.now()
-    return HttpResponse(' la hora del servidor es {now}'.format(now =str(now)))
+    now = datetime.now().strftime('%b %dth , %T - %H:%M hrs')
+    return HttpResponse(' la hora del servidor es {now}'.format(now=str(now)))
+    
+
+def hi(request):
+    numbers_string = request.GET['numbers'].split(',')
+    print(numbers_string)
+    numbers_string2 = request.GET['numbers']
+    print(numbers_string2)
+    numbers_integer = list(map(int, numbers_string))
+    numbers_integer.sort()
+    return JsonResponse(numbers_integer, safe =False)
