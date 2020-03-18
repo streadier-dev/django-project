4874c3fdaabb999a72feaba660a7a03b717f4a0d
gabriel <gabriel.faure@readinessit.com>
2020-03-18 01:17:41 -0300

    añadir templete de registro y logica de registro en la vista de users

--- users/views.py
+++ users/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 
+#Models
+from django.contrib.auth.models import User
+from users.models import Profile
+
+#Exception
+
+from django.db.utils import IntegrityError
 
 def login_view(request):
     if request.method == 'POST':
@@ -21,3 +28,28 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('login')
+
+def signup_view(request):
+    if request.method == 'POST':
+         username = request.POST['username']
+         passwd = request.POST['passwd']
+         passwd_confirmation = request.POST['passwd_confirmation']
+
+         if passwd != passwd_confirmation:
+             return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
+         try:
+            user = User.objects.create_user(username=username, password=passwd)
+         except IntegrityError:
+             return render(request, 'users/signup.html', {'error': 'Username is already in use'})
+
+         user.first_name = request.POST['first_name']
+         user.last_name = request.POST['last_name']
+         user.email = request.POST['email']
+         user.save()
+
+         profile = Profile(user=user)
+         profile.save()
+
+         return redirect('login')
+
+    return render(request, 'users/signup.html')
