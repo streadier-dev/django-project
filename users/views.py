03725e8fc38be561526f8cb5b25b6dc43a4f479f
gabriel <gabriel.faure@readinessit.com>
2020-03-19 19:14:17 -0300

    form de registro y salvar los datos (forms.py post) y archivo views post modificado para registro

--- users/views.py
+++ users/views.py
@@ -1,18 +1,14 @@
 from django.shortcuts import render, redirect
-# Create your views here.
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
-
-#Models
-from django.contrib.auth.models import User
-from users.models import Profile
 
 #Exception
 
 from django.db.utils import IntegrityError
 
 #Forms
-from users.forms import ProfileForm
+
+from users.forms import ProfileForm, SignupFrom
 
 @login_required
 def update_profile(request):
@@ -63,25 +59,14 @@ def logout_view(request):
 
 def signup_view(request):
     if request.method == 'POST':
-         username = request.POST['username']
-         passwd = request.POST['passwd']
-         passwd_confirmation = request.POST['passwd_confirmation']
-
-         if passwd != passwd_confirmation:
-             return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
-         try:
-            user = User.objects.create_user(username=username, password=passwd)
-         except IntegrityError:
-             return render(request, 'users/signup.html', {'error': 'Username is already in use'})
-
-         user.first_name = request.POST['first_name']
-         user.last_name = request.POST['last_name']
-         user.email = request.POST['email']
-         user.save()
-
-         profile = Profile(user=user)
-         profile.save()
-
-         return redirect('login')
-
-    return render(request, 'users/signup.html')
+        form = SignupFrom(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+            form = SignupFrom()
+    return render(
+            request=request,
+            template_name='users/signup.html',
+            context={'form':form}
+        )
